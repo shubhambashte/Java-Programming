93b818dbeae5761403cf785f767842605c6fa46f
shubhambashte <shubhamrbashte@gmail.com>
2026-03-18 20:13:48 +0530

    Predefined

---
+++ Predefined.java
@@ -0,0 +1,6 @@
+public class Predefined {
+    public static void main(String[] args) {
+        Predefined obj = new Predefined();
+        System.out.println(obj.hashCode()); // Predefined method
+    }
+}
