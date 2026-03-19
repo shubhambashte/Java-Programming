94623defd3e682d6149aacc6dead18f7862a79b3
shubhambashte <shubhamrbashte@gmail.com>
2026-03-19 20:00:18 +0530

    String

---
+++ String.java
@@ -0,0 +1,13 @@
+public class String {
+
+    // Main Function
+    public static void main(String args[])
+    {
+
+        // creating Java string using a new keyword
+        String str = new String();
+
+        System.out.println(str);
+    }
+}
+
