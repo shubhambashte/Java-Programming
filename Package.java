d0fd5b41dd912fa081cda98be877873d012a2c6e
shubhambashte <shubhamrbashte@gmail.com>
2026-03-20 20:10:08 +0530

    Package

---
+++ Package.java
@@ -0,0 +1,21 @@
+// Package
+// Built-in package 
+/*
+
+import java.util.Random;   // built-in package
+
+public class Package{
+    
+    public static void main(String[] args) {
+        
+        // using Random class
+        Random rand = new Random();   
+
+        // generates a number between 0–99
+        int number = rand.nextInt(100);  
+
+        System.out.println("Random number: " + number);
+    }
+}
+     */
+
